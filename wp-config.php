f391979c2c417624e0c00b191e69c216d092c71d
Vitaliy Kulesh <vkulesh@gmail.com>
2026-03-19 10:45:38 -0400

    Add files via upload

---
+++ wp-config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* That's all, stop editing! Happy publishing. */
+
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
+
+}
+require_once __DIR__ . '/../configs/wp-config-hosting.php';
+
+require_once ABSPATH . 'wp-settings.php';
